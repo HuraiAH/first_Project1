6395681a3bdc857f03a45f441911a6599d83d4ed
HuraiAH <115168405+HuraiAH@users.noreply.github.com>
2022-11-16 22:45:50 +0600

    Add files via upload

---
+++ js/color.js
@@ -0,0 +1,8 @@
+const colors= document.getElementsByClassName('colors');
+for( let i= 0; i< colors.length; i++){
+    colors[i].addEventListener('click', changeColor)
+}
+function changeColor(){
+    let color= this.getAttribute('data-color');
+    document.documentElement.style.setProperty( '--themes_color',  color);
+}
